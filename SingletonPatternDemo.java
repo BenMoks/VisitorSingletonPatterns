9d64c8f530ce76eeb1c24b1f8f6775e7cca2bd83
BenMoks <ben107m@gmail.com>
2024-12-09 23:26:26 -0500

    Delete SingletonPatternDemo.java

--- SingletonPatternDemo.java
+++
@@ -1,9 +0,0 @@
-public class SingletonPatternDemo {
-    public static void main(String[] args) {
-        // Get the only instance of SingleObject
-        SingleObject object = SingleObject.getInstance();
-
-        // Show the message
-        object.showMessage();
-    }
-}
