422c5351b107c875c61c3dcadb2ffdb9cc1a9578
BenMoks <ben107m@gmail.com>
2024-12-13 20:55:15 -0500

    Add files via upload

---
+++ Part 2/Part 2.java
@@ -0,0 +1,64 @@
+public interface ComputerPart {
+    void accept(ComputerPartVisitor computerPartVisitor);
+    }
+    public class Mouse implements ComputerPart {
+    @Override
+    public void accept(ComputerPartVisitor computerPartVisitor) {
+    computerPartVisitor.visit(this);
+    }
+    }
+    public class Keyboard implements ComputerPart {
+    @Override
+    public void accept(ComputerPartVisitor computerPartVisitor) {
+    computerPartVisitor.visit(this);
+    }
+    }
+    public class Monitor implements ComputerPart {
+    @Override
+    public void accept(ComputerPartVisitor computerPartVisitor) {
+    computerPartVisitor.visit(this);
+    }
+    }
+    public class Computer implements ComputerPart {
+    ComputerPart[] parts;
+    public Computer() {
+    parts = new ComputerPart[] {new Mouse(), new Keyboard(), new Monitor()};
+    }
+    @Override
+    public void accept(ComputerPartVisitor computerPartVisitor) {
+    for (ComputerPart part : parts) {
+    part.accept(computerPartVisitor);
+    }
+    computerPartVisitor.visit(this);
+    }
+    }
+    public interface ComputerPartVisitor {
+    void visit(Computer computer);
+    void visit(Mouse mouse);
+    void visit(Keyboard keyboard);
+    void visit(Monitor monitor);
+    }
+    public class ComputerPartDisplayVisitor implements ComputerPartVisitor {
+    @Override
+    public void visit(Computer computer) {
+    System.out.println("Displaying Computer.");
+    }
+    @Override
+    public void visit(Mouse mouse) {
+    System.out.println("Displaying Mouse.");
+    }
+    @Override
+    public void visit(Keyboard keyboard) {
+    System.out.println("Displaying Keyboard.");
+    }
+    @Override
+    public void visit(Monitor monitor) {
+    System.out.println("Displaying Monitor.");
+    }
+    }
+    public class VisitorPatternDemo {
+    public static void main(String[] args) {
+    ComputerPart computer = new Computer();
+    computer.accept(new ComputerPartDisplayVisitor());
+    }
+    }
